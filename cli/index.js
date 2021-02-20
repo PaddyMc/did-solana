f7e574eee4deb45c8784827beed04716ec139aef
PaddyMc <paddymchale@hotmail.com>
2021-02-20 14:06:06 +0000

    feat(did): round trip with u8 ints

--- cli/index.js
+++ cli/index.js
@@ -1,41 +1,123 @@
-var solana_web3 = require("@solana/web3.js");
+const solana_web3 = require("@solana/web3.js");
+const lo = require("buffer-layout");
 
-const testMemo = async (connection, account) => {
+const createDid = async (connection, programId) => {
+  const lamports = 10 * 1000000000;
+  const numBytes = 5;
+
+  const account = new solana_web3.Account();
+  await connection.requestAirdrop(account.publicKey, lamports);
+
+  const dataAccount = new solana_web3.Account();
+
+  const rentExemption = await connection.getMinimumBalanceForRentExemption(
+    numBytes
+  );
+
+  const transaction = new solana_web3.Transaction();
+  transaction.add(
+    solana_web3.SystemProgram.createAccount({
+      fromPubkey: account.publicKey,
+      newAccountPubkey: dataAccount.publicKey,
+      lamports: rentExemption,
+      space: numBytes,
+      programId: programId,
+    })
+  );
+ 
+
+
+const ds = lo.cstr();
+const b = Buffer.alloc(8);
+console.log(ds.encode('hi!', b) === 3 + 1);
+const slen = ds.getSpan(b);
+console.log(slen)
+
+  const dataLayout = lo.struct([
+    lo.u8("instruction"),
+    lo.u8("context"),
+    lo.u8("id"),
+    lo.u8("aka"),
+    lo.u8("authentication"),
+    lo.u8("services"),
+  ]);
+  const data = Buffer.alloc(dataLayout.span);
+  dataLayout.encode(
+    {
+      instruction: 0, // InitializeCreateDid instruction
+      context: 1,
+      id: 2,
+      aka: 3,
+      authentication: 4,
+      services: 5,
+    },
+    data
+  );
+  console.log(data)
+
   const instruction = new solana_web3.TransactionInstruction({
-    keys: [],
-    programId: new solana_web3.PublicKey(
-      "8Ku3PKjy23eyPoMdAtswivoBMdL2i28jWfcox4e46mza"
-    ),
-    data: Buffer.from("cztest"),
+    keys: [
+      {
+        pubkey: account.publicKey,
+        isSigner: true,
+        isWritable: false,
+      },
+      {
+        pubkey: dataAccount.publicKey,
+        isSigner: false,
+        isWritable: true,
+      },
+    ],
+    programId: programId,
+    data: data,
   });
-  console.log("account:", account.publicKey.toBase58());
+
+  transaction.add(instruction);
 
   const result = await solana_web3
     .sendAndConfirmTransaction(
       connection,
-      new solana_web3.Transaction().add(instruction),
-      [account],
+      transaction,
+      [account, dataAccount],
       {
+        confirmations: 1,
         skipPreflight: true,
-        commitment: "singleGossip",
       }
     )
-    .catch((e) => {
-      console.log("error", e);
-    });
-  console.log(result)
+
+  await getAccountInfo(connection, dataAccount.publicKey);
+};
+
+const getAccountInfo = async (connection, pk) => {
+  if (!pk) {
+    console.log("pk not provided");
+    process.exit(1);
+  }
+
+  console.log("Inspecting pk:", pk.toString());
+
+  let account = await connection.getAccountInfo(pk);
+
+  if (!account) {
+    console.log("Account not found on chain");
+    process.exit(1);
+  }
+  console.log(account);
+
+  let owner = new solana_web3.PublicKey(account.owner._bn);
+
+  console.log("Owner PubKey:", owner.toString());
 };
 
 const main = async () => {
   connection = new solana_web3.Connection(
-    "https://devnet.solana.com",
+    "http://localhost:8899",
     "singleGossip"
   );
-  const account = new solana_web3.Account();
-  const lamports = 10 * 1000000000;
-  await connection.requestAirdrop(account.publicKey, lamports);
+  console.log(process.argv[2]);
+  let programId = new solana_web3.PublicKey(process.argv[2]);
 
-  testMemo(connection, account);
+  await createDid(connection, programId);
 };
 
 main();
--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1,12 +1,11 @@
 //! Program instruction processor
 use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    log::{sol_log, sol_log_slice},
+    log::sol_log,
     msg,
     program_error::ProgramError,
-    program_option::COption,
     program_pack::{Pack, Sealed},
     pubkey::Pubkey,
 };
@@ -20,20 +19,55 @@ pub fn process_instruction(
     msg!("Program started:");
     let instruction = DidInstruction::unpack(instruction_data)?;
     match instruction {
-        DidInstruction::Did { context, services } => {
-            msg!("Instruction: Did");
-            create_did(program_id, accounts, context, services)
-        }
+        DidInstruction::CreateDid {
+            context,
+            id,
+            aka,
+            authentication,
+            services,
+        } => create_did(
+            program_id,
+            accounts,
+            context,
+            id,
+            aka,
+            authentication,
+            services,
+        ),
     }
 }
 
 /// Create a did document
 pub fn create_did(
-    program_id: &Pubkey,
+    _: &Pubkey,
     accounts: &[AccountInfo],
     context: u8,
+    id: u8,
+    aka: u8,
+    authentication: u8,
     services: u8,
 ) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+    let _ = next_account_info(account_info_iter)?;
+    let did_info = next_account_info(account_info_iter)?;
+    did_info.key.log();
+    msg!("Creating: Did");
+    //let mut did = DidDocument::unpack_unchecked(&did_info.data.borrow())?;
+
+    let did = DidDocument {
+        context,
+        id,
+        aka,
+        authentication,
+        services,
+    };
+    msg!("Updating account data");
+
+    msg!("Updating account data");
+    DidDocument::pack(did, &mut did_info.data.borrow_mut())?;
+
+    msg!("Created: Did");
+
     Ok(())
 }
 
@@ -46,107 +80,160 @@ pub fn create_did(
 #[derive(Debug, PartialEq)]
 pub enum DidInstruction {
     /// Initilaizes a new Did
-    Did {
-        /// context
+    CreateDid {
+        /// context refers to the url of the w3c spec
         context: u8,
-        /// services
+        /// id refers to the id of the did document
+        id: u8,
+        /// aka is used for vanity names for associated public keys
+        aka: u8,
+        /// authentication is a list of public keys associated with a did document
+        authentication: u8,
+        /// services represent ways to get data about the did subject
         services: u8,
     },
 }
+
 impl DidInstruction {
     /// Unpacks a byte buffer into a [DidInstruction](enum.DidInstruction.html).
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        use ProgramError::InvalidAccountData;
-
-        let (tag, rest) = input.split_first().ok_or(InvalidAccountData)?;
-        sol_log("fucking stupid program");
-        msg!(&tag.to_string());
-        msg!(std::str::from_utf8(&rest).unwrap());
+        use ProgramError::InvalidArgument;
+        let (tag, rest) = input.split_first().ok_or(InvalidArgument)?;
         Ok(match &tag {
             0 => {
-                sol_log("000000000000000");
-                let context: u8 = 42;
-                let services: u8 = 42;
-                Self::Did { context, services }
+                sol_log("Decoding instruction_data");
+                msg!(std::str::from_utf8(rest).unwrap());
+                let (&context, _rest) = rest.split_first().ok_or(InvalidArgument)?;
+                let (&id, _rest) = _rest.split_first().ok_or(InvalidArgument)?;
+                let (&aka, _rest) = _rest.split_first().ok_or(InvalidArgument)?;
+                let (&authentication, _rest) = _rest.split_first().ok_or(InvalidArgument)?;
+                let (&services, _rest) = _rest.split_first().ok_or(InvalidArgument)?;
+                Self::CreateDid {
+                    context,
+                    id,
+                    aka,
+                    authentication,
+                    services,
+                }
             }
-            _ => return Err(ProgramError::InvalidAccountData.into()),
+            _ => return Err(ProgramError::InvalidArgument.into()),
         })
     }
 
-    fn unpack_pubkey(input: &[u8]) -> Result<(Pubkey, &[u8]), ProgramError> {
-        if input.len() >= 32 {
-            let (key, rest) = input.split_at(32);
-            let pk = Pubkey::new(key);
-            Ok((pk, rest))
-        } else {
-            Err(ProgramError::InvalidAccountData.into())
-        }
-    }
+    //    fn unpack_pubkey(input: &[u8]) -> Result<(Pubkey, &[u8]), ProgramError> {
+    //        if input.len() >= 32 {
+    //            let (key, rest) = input.split_at(32);
+    //            let pk = Pubkey::new(key);
+    //            Ok((pk, rest))
+    //        } else {
+    //            Err(ProgramError::InvalidAccountData.into())
+    //        }
+    //    }
 
-    fn unpack_pubkey_option(input: &[u8]) -> Result<(COption<Pubkey>, &[u8]), ProgramError> {
-        match input.split_first() {
-            Option::Some((&0, rest)) => Ok((COption::None, rest)),
-            Option::Some((&1, rest)) if rest.len() >= 32 => {
-                let (key, rest) = rest.split_at(32);
-                let pk = Pubkey::new(key);
-                Ok((COption::Some(pk), rest))
-            }
-            _ => Err(ProgramError::InvalidAccountData),
-        }
-    }
-
-    fn pack_pubkey_option(value: &COption<Pubkey>, buf: &mut Vec<u8>) {
-        match *value {
-            COption::Some(ref key) => {
-                buf.push(1);
-                buf.extend_from_slice(&key.to_bytes());
-            }
-            COption::None => buf.push(0),
-        }
-    }
+    //    fn unpack_pubkey_option(input: &[u8]) -> Result<(COption<Pubkey>, &[u8]), ProgramError> {
+    //        match input.split_first() {
+    //            Option::Some((&0, rest)) => Ok((COption::None, rest)),
+    //            Option::Some((&1, rest)) if rest.len() >= 32 => {
+    //                let (key, rest) = rest.split_at(32);
+    //                let pk = Pubkey::new(key);
+    //                Ok((COption::Some(pk), rest))
+    //            }
+    //            _ => Err(ProgramError::InvalidAccountData),
+    //        }
+    //    }
+    //
+    //    fn pack_pubkey_option(value: &COption<Pubkey>, buf: &mut Vec<u8>) {
+    //        match *value {
+    //            COption::Some(ref key) => {
+    //                buf.push(1);
+    //                buf.extend_from_slice(&key.to_bytes());
+    //            }
+    //            COption::None => buf.push(0),
+    //        }
+    //    }
 }
 
 ///
 /// State
 ///
 
+/// The did repository should store all data
+/// related to a did document.
+///
+/// Spec: https://w3c.github.io/did-core/
+/// {
+///  "@context": "https://www.w3.org/ns/did/v1",
+///  "id": "did:sol:123456789abcdefghi",
+///  "aka": "JonBonJovi",
+///  "authentication": [{
+///    "id": "did:sol:123456789abcdefghi#keys-1",
+///    "type": "Ed25519VerificationKey2020",
+///    "controller": "did:sol:123456789abcdefghi",
+///    "publicKeyMultibase": "zH3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+///  }],
+///  "service": [{
+///   "id":"did:sol:123#linked-data",
+///   "type": "LinkedData",
+///   "service": "zH3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+///   }],
+/// }
+
 /// DidDocument data.
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 pub struct DidDocument {
-    /// context
-    pub context: u8,
-    /// authentication
-    pub authentication: COption<Pubkey>,
-    /// services
-    pub services: u8,
+    /// context refers to the url of the w3c spec
+    context: u8,
+    /// id refers to the id of the did document
+    id: u8,
+    /// aka is used for vanity names for associated public keys
+    aka: u8,
+    /// authentication is a list of public keys associated with a did document
+    authentication: u8,
+    /// services represent ways to get data about the did subject
+    services: u8,
 }
 impl Sealed for DidDocument {}
 impl Pack for DidDocument {
-    const LEN: usize = 38;
+    const LEN: usize = 5;
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
-        let src = array_ref![src, 0, 38];
-        let (context, authentication, services) = array_refs![src, 1, 36, 1];
-        let context = u8::from_le_bytes(*context);
-        let authentication = unpack_coption_key(authentication)?;
-        let services = u8::from_le_bytes(*services);
+        let src = array_ref![src, 0, 5];
+        //let (context, authentication, services) = array_refs![src, 1, 36, 1];
+        let (context, id, aka, authentication, services) = array_refs![src, 1, 1, 1, 1, 1];
+        let context = context[0];
+        let id = id[0];
+        let aka = aka[0];
+        let authentication = authentication[0];
+        let services = services[0];
+
+        //    let authentication = unpack_coption_key(authentication)?;
         Ok(DidDocument {
             context,
+            id,
+            aka,
             authentication,
             services,
         })
     }
+
     fn pack_into_slice(&self, dst: &mut [u8]) {
-        let dst = array_mut_ref![dst, 0, 38];
-        let (context_dst, authentication_dst, services_dst) = array_refs![dst, 1, 36, 1];
+        let dst = array_mut_ref![dst, 0, 5];
+        let (context_dst, id_dst, aka_dst, authentication_dst, services_dst) =
+            mut_array_refs![dst, 1, 1, 1, 1, 1];
         let &DidDocument {
             context,
-            ref authentication,
+            id,
+            aka,
+            authentication,
             services,
         } = self;
+        sol_log("Packing did document into account_data");
         //pack_coption_key(authentication_dst, authentication);
-        //*context_dst = context.to_le_bytes();
-        //*services_dst = services.to_le_bytes();
+        context_dst[0] = context;
+        id_dst[0] = id;
+        aka_dst[0] = aka;
+        authentication_dst[0] = authentication;
+        services_dst[0] = services;
     }
 }
 
@@ -164,11 +251,11 @@ impl Pack for DidDocument {
 //    }
 //}
 
-fn unpack_coption_key(src: &[u8; 36]) -> Result<COption<Pubkey>, ProgramError> {
-    let (tag, body) = array_refs![src, 4, 32];
-    match *tag {
-        [0, 0, 0, 0] => Ok(COption::None),
-        [1, 0, 0, 0] => Ok(COption::Some(Pubkey::new_from_array(*body))),
-        _ => Err(ProgramError::InvalidAccountData),
-    }
-}
+//fn unpack_coption_key(src: &[u8; 36]) -> Result<COption<Pubkey>, ProgramError> {
+//    let (tag, body) = array_refs![src, 4, 32];
+//    match *tag {
+//        [0, 0, 0, 0] => Ok(COption::None),
+//        [1, 0, 0, 0] => Ok(COption::Some(Pubkey::new_from_array(*body))),
+//        _ => Err(ProgramError::InvalidAccountData),
+//    }
+//}
